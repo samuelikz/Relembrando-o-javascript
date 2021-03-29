07b148a5552f5ad9992fc67fcfad14e31fa01827
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-03-29 16:00:07 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+/*
+    * Função e um tipo de dados estrutural 
+*/
+
+function createPharses(){
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+//rodar a função, execute, run, call, invoke
+
+createPharses()
+
+console.log('fim do programa')
