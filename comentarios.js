a882b0613657ec4cd38cae6e8dbcb79ce7985010
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-03-29 11:21:17 -0300

    Add files via upload

---
+++ comentarios.js
@@ -0,0 +1,13 @@
+// Aqui eu tenho um comentário em linha
+
+
+/*
+    Aqui eu tenho um comentario 
+
+    com varias 
+
+    linhas
+*/
+
+console.log('Bom vindos ao Starter')
+console.log('Não iria ignorar')
