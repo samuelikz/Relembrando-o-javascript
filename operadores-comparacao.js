dec8e4e67034ad003acf03455655b75d68df6e31
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-03-30 11:09:25 -0300

    Add files via upload

---
+++ operadores-comparacao.js
@@ -0,0 +1,16 @@
+//Operadores de comparação
+// Irá comparar valores e retornar um Boolean como resposta á comparação
+
+let one = 1
+let two = 2
+
+console.log(one == 1) // true
+console.log(one == two) // false
+// ==  igual
+// !=  diferente
+// === estritamente igual
+// !== estritamente diferente
+// >   Maior
+// >=  Maior igual
+// <   Menor
+// <=  Menor igual
